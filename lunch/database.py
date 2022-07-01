49db47ffb2b16d69d015e1e9640627d0a3c9fa76
Michał 'Khorne' Lowas-Rzechonek <michal@rzechonek.net>
2022-07-01 11:22:41 +0200

    Add the first model and migration

--- lunch/database.py
+++ lunch/database.py
@@ -1,5 +1,12 @@
+from sqlalchemy import Column, String
 from sqlalchemy.orm import declarative_base
 
 DATABASE = "sqlite:///lunch.sqlite"
 
 Base = declarative_base()
+
+
+class Feed(Base):
+    __tablename__ = "feed"
+
+    url = Column(String, primary_key=True)
